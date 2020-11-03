df346f7d563ddb4c0fffeffcea339f904c6dd12f
alfredoFaro98 <55789420+alfredoFaro98@users.noreply.github.com>
2020-11-03 12:53:21 +0100

    Add files via upload

---
+++ CudaProgramming/1 - firstProof.cu
@@ -0,0 +1,13 @@
+#include<cuda.h>
+#include<stdio.h>
+#include<ctime>
+using namespace std;
+/*Base configuration of normal cuda script -> try this to set your configuration*/
+/*To run this I have access on workstation university where there are code cuda configuration*/
+/*I use MobaXTerm to create the ssh connection and visual studio code or nano tool to modify the code*/
+
+int main()
+{
+    printf("Configuration completed? Yes my friend");
+    return 0;
+}
